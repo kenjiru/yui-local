fab71ec336ae9d47acaced69e1afd15c3c4a1058
kenjiru <kenjiru.ro@gmail.com>
2012-07-10 15:50:13 +0200

    Made all the paths in the configuration relative.

--- js/yui-configuration.js
+++ js/yui-configuration.js
@@ -1,19 +1,25 @@
 YUI_config = {
     filter: "raw",
-    base: "/kenjiru/yui-local/workspace/lib/yui3/build/",
-    root: "kenjiru/yui-local/workspace/lib/yui3/build/",
+    root: "lib/yui3/build/",
     comboBase: "/combo?",
     combine: false,
     groups: {
         // set up for locally served gallery
         gallery: {
-            base: "/kenjiru/yui-local/workspace/lib/yui3-gallery/build/",
-            root: "kenjiru/yui-local/workspace/lib/yui3-gallery/build/",
+            root: "lib/yui3-gallery/build/",
             comboBase: "/combo?",
             combine: false,
             patterns: {
                 "gallery-": {},
                 "gallerycss-": { type: "css" }
+            }
+        },
+        wfo: {
+            base: "wfo/build/",
+            comboBase: "/combo?",
+            combine: false,
+            modules : {
+                ckeditor : { }
             }
         }
     }
